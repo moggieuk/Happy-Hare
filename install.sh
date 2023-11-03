7fa0babdd0460e150bec285b32a930cab1884455
Moggie <moggieuk@gmail.com>
2023-11-03 12:28:03 +0700

    Added detection of upgrade to v2.2 without running ./install.sh

--- install.sh
+++ install.sh
@@ -183,7 +183,7 @@ cleanup_manual_stepper_version() {
     if [ -d "${KLIPPER_HOME}/klippy/extras" ]; then
         rm -f "${KLIPPER_HOME}/klippy/extras/manual_mh_stepper.py"
         rm -f "${KLIPPER_HOME}/klippy/extras/manual_extruder_stepper.py"
-        rm -f "${KLIPPER_HOME}/klippy/extras/mmu_config_setup.py"
+        # Used as upgrade reminder rm -f "${KLIPPER_HOME}/klippy/extras/mmu_config_setup.py"
     fi
 
     # Upgrade mmu_hardware.cfg...
@@ -357,6 +357,8 @@ read_previous_config() {
         fi
         if [ ! "${encoder_parking_distance}" == "" ]; then
             gate_parking_distance=${encoder_parking_distance}
+        else
+            gate_parking_distance=23
         fi
         if [ ! "${encoder_load_retries}" == "" ]; then
             gate_load_retries=${encoder_load_retries}
@@ -1032,7 +1034,7 @@ questionaire() {
                     ;;
             esac
 
-            echo -e "${PROMPT}    Would you like to include legacy ERCF_ command compatibility module${INPUT}"
+            echo -e "${PROMPT}    Would you like to include legacy ERCF_ command set compatibility module${INPUT}"
             yn=$(prompt_yn "    Include legacy ERCF command set")
             echo
             case $yn in
@@ -1193,7 +1195,7 @@ if [ "$UNINSTALL" -eq 0 ]; then
     echo -e "${INFO}"
     echo '(\_/)'
     echo '( *,*)'
-    echo '(")_(") MMU Ready'
+    echo '(")_(") Happy Hare Ready'
     echo
 else
     echo -e "${EMPHASIZE}"
@@ -1201,6 +1203,6 @@ else
     echo -e "${INFO}"
     echo '(\_/)'
     echo '( v,v)'
-    echo '(")^(") MMU Unready'
+    echo '(")^(") Very Unappy Hare'
     echo
 fi
