d7fa2f3fd9b6ad2589610468c8bdf46eb84f1669
Moggie <moggieuk@gmail.com>
2023-12-08 12:14:13 +0700

    update to match latest klipper change to toolhead class

--- extras/mmu_toolhead.py
+++ extras/mmu_toolhead.py
@@ -21,26 +21,24 @@ import stepper, chelper, toolhead
 from extras.homing import Homing, HomingMove
 from kinematics.extruder import PrinterExtruder, DummyExtruder, ExtruderStepper
 
-SDS_CHECK_TIME = 0.001 # step+dir+step filter in stepcompress.c
-
 # Main code to track events (and their timing) on the MMU Machine implemented as additional "toolhead"
 # (code pulled from toolhead.py)
 class MmuToolHead(toolhead.ToolHead, object):
     def __init__(self, config):
+
         self.printer = config.get_printer()
         self.reactor = self.printer.get_reactor()
         self.all_mcus = [m for n, m in self.printer.lookup_objects(module='mcu')]
-        self.mcu = self.all_mcus[0]
-        self.can_pause = True
-        if self.mcu.is_fileoutput():
-            self.can_pause = False
-        self.move_queue = toolhead.MoveQueue(self) # Use base class MoveQueue
-        self.gear_motion_queue = self.extruder_synced_to_gear = None # For bi-directional syncing of gear and extruder
-        self.prev_rail_steppers = self.prev_g_sk = self.prev_sk = self.prev_trapq = None
-        self.commanded_pos = [0., 0., 0., 0.]
-        self.printer.register_event_handler("klippy:shutdown", self._handle_shutdown)
 
-        # Velocity and acceleration control
+        self.mcu = self.all_mcus[0]
+        self.move_queue = toolhead.MoveQueue(self) # Happy Hare: Use base class MoveQueue
+        self.move_queue.set_flush_time(toolhead.BUFFER_TIME_HIGH) # Happy Hare: Use base class
+        self.commanded_pos = [0., 0., 0., 0.]
+
+        self.gear_motion_queue = self.extruder_synced_to_gear = None # Happy Hare: For bi-directional syncing of gear and extruder
+        self.prev_rail_steppers = self.prev_g_sk = self.prev_sk = self.prev_trapq = None # Happy Hare: for stepper switching
+
+        # MMU velocity and acceleration control
         self.gear_max_velocity = config.getfloat('gear_max_velocity', 300, above=0.)
         self.gear_max_accel = config.getfloat('gear_max_accel', 500, above=0.)
         self.selector_max_velocity = config.getfloat('selector_max_velocity', 250, above=0.)
@@ -55,23 +53,27 @@ class MmuToolHead(toolhead.ToolHead, object):
         self.square_corner_velocity = config.getfloat('square_corner_velocity', 5., minval=0.)
         self.junction_deviation = 0.
         self._calc_junction_deviation()
-        # Print time tracking
-        self.buffer_time_low = config.getfloat('buffer_time_low', 1.000, above=0.)
-        self.buffer_time_high = config.getfloat('buffer_time_high', 2.000, above=self.buffer_time_low)
-        self.buffer_time_start = config.getfloat('buffer_time_start', 0.250, above=0.)
-        self.move_flush_time = config.getfloat('move_flush_time', 0.050, above=0.)
-        self.print_time = 0.
-        self.special_queuing_state = "Flushed"
-        self.need_check_stall = -1.
-        self.flush_timer = self.reactor.register_timer(self._flush_handler)
-        self.move_queue.set_flush_time(self.buffer_time_high)
-        self.idle_flush_print_time = 0.
+
+        # Input stall detection
+        self.check_stall_time = 0.
         self.print_stall = 0
+        # Input pause tracking
+        self.can_pause = True
+        if self.mcu.is_fileoutput():
+            self.can_pause = False
+        self.need_check_pause = -1.
+        # Print time tracking
+        self.print_time = 0.
+        self.special_queuing_state = "NeedPrime"
+        self.priming_timer = None
         self.drip_completion = None
+        # Flush tracking
+        self.flush_timer = self.reactor.register_timer(self._flush_handler)
+        self.do_kick_flush_timer = True
+        self.last_flush_time = self.need_flush_time = 0.
         # Kinematic step generation scan window time tracking
-        self.kin_flush_delay = SDS_CHECK_TIME
+        self.kin_flush_delay = toolhead.SDS_CHECK_TIME # Happy Hare: Use base class
         self.kin_flush_times = []
-        self.force_flush_time = self.last_kin_move_time = 0.
         # Setup iterative solver
         ffi_main, ffi_lib = chelper.get_ffi()
         self.trapq = ffi_main.gc(ffi_lib.trapq_alloc(), ffi_lib.trapq_free)
@@ -82,6 +84,8 @@ class MmuToolHead(toolhead.ToolHead, object):
         gcode = self.printer.lookup_object('gcode')
         self.Coord = gcode.Coord
         self.extruder = DummyExtruder(self.printer)
+
+        self.printer.register_event_handler("klippy:shutdown", self._handle_shutdown)
 
         # Setup extruder kinematics for when gear rail is synced to extruder
         ffi_main, ffi_lib = chelper.get_ffi()
@@ -94,6 +98,7 @@ class MmuToolHead(toolhead.ToolHead, object):
         # Create MMU kinematics
         try:
             self.kin = MmuKinematics(self, config)
+            self.all_gear_rail_steppers = self.kin.rails[1].get_steppers()
         except config.error as e:
             raise
         except self.printer.lookup_object('pins').error as e:
@@ -137,6 +142,34 @@ class MmuToolHead(toolhead.ToolHead, object):
 
     def get_gear_limits(self):
         return self.gear_max_velocity, self.gear_max_accel
+
+    def select_gear_steppers(self, selected_steppers): # TODO untested WIP
+        # Unsync first to simplify transition
+        gear_motion_queue = self.gear_motion_queue
+        extruder_synced_to_gear = self.extruder_synced_to_gear
+        self.sync_gear_to_extruder(None)
+        self.sync_extruder_to_gear(None)
+
+        # Activate only the desired gear(s)
+        printer_toolhead = self.printer.lookup_object('toolhead')
+        printer_toolhead.flush_step_generation()
+        self.flush_step_generation()
+        gear_rail = self.get_kinematics().rails[1]
+        g_pos = gear_rail.get_commanded_position()
+        gear_rail.steppers = []
+        # TODO need to handle step generators? or can they safety always be assigned to toolhead?
+        for s in self.all_gear_rail_steppers:
+            if s.get_name() in selected_steppers:
+                gear_rail.steppers.append(s)
+        if not gear_rail.steppers:
+            raise self.printer.command_error("None of these `%s` gear steppers where found!" % selected_steppers)
+        gear_rail.set_position([g_pos, 0., 0.])
+
+        # Restore previous synchronization state if any with new gear steppers
+        if gear_motion_queue:
+            self.sync_gear_to_extruder(gear_motion_queue)
+        elif extruder_synced_to_gear:
+            self.sync_extruder_to_gear(extruder_synced_to_gear)
 
     # Is gear rail synced to extruder (for in print syncing)
     def is_gear_synced_to_extruder(self):
@@ -553,11 +586,11 @@ class MmuPrinterRail(stepper.PrinterRail, object):
         def add_stepper(self, *args, **kwargs):
             pass
 
-# Wrapper for dual stepper motor support
+# Wrapper for multiple stepper motor support
 def MmuLookupMultiRail(config, need_position_minmax=True, default_position_endstop=None, units_in_radians=False):
     rail = MmuPrinterRail(config, need_position_minmax=need_position_minmax, default_position_endstop=default_position_endstop, units_in_radians=units_in_radians)
-    for i in range(1, 99):
-        if not config.has_section(config.get_name() + str(i)):
+    for i in range(0, 23):
+        if not config.has_section(config.get_name() + "_" + str(i)):
             break
         rail.add_extra_stepper(config.getsection(config.get_name() + str(i)))
     return rail
