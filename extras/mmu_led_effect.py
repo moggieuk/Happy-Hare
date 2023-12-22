304af3152b7a51bb3cd4fffa619c54b620957317
Moggie <moggieuk@gmail.com>
2023-12-22 13:08:44 +0700

    Allow led_effects to be optional; improved error message; fixed installer where user had modifed example macros

--- extras/mmu_led_effect.py
+++ extras/mmu_led_effect.py
@@ -12,18 +12,21 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 #
 import logging
-from extras.led_effect import ledEffect
 
-class MmuLedEffect(ledEffect, object):
+class MmuLedEffect:
 
     first_led_index = None # Starting LED index (must be the same and also used for configuring the LED macro)
     last_led_index = None
 
     def __init__(self, config):
         self.printer = config.get_printer()
-        leds = config.get('leds').split(' ', 1)[0]
-        strip = leds.replace(':', ' ')
-        first, last = map(int, config.get('leds').split(' ', 1)[1].strip('()').split('-'))
+        try:
+            leds = config.get('leds').split(' ', 1)[0]
+            strip = leds.replace(':', ' ')
+            first, last = map(int, config.get('leds').split(' ', 1)[1].strip('()').split('-'))
+        except Exception as e:
+            raise config.error("Invalid 'leds' specification: %s. Exception: %s" % (config.get('leds'), str(e)))
+        
         if not MmuLedEffect.first_led_index:
             MmuLedEffect.first_led_index = first
         elif first != MmuLedEffect.first_led_index:
@@ -40,17 +43,27 @@ class MmuLedEffect(ledEffect, object):
         config.fileconfig.set(config.get_name(), 'leds', new_leds)
 
         # Create the combined gate_effects effect
-        super(MmuLedEffect, self).__init__(config)
+        section_to = config.get_name()[4:]
+        config.fileconfig.add_section(section_to)
+        new_section = config.getsection(section_to)
+        items = config.fileconfig.items(config.get_name())
+        for item in items:
+            _ = config.get(item[0])
+            config.fileconfig.set(section_to, item[0], item[1])
+        try:
+            _ = self.printer.load_object(config, new_section.get_name())
+        except Exception as e:
+            raise config.error("Unable to create led effect`. It is likely you don't have the 'led_effect' klipper module installed. Exception: %s" % str(e))
 
         # Create individual effects for gate and exit leds
         for i in range(first, last + 1):
             new_section = self._add_config_section(config, config.get_name(), i)
-            _ = ledEffect(new_section)
+            _ = self.printer.load_object(config, new_section.get_name())
 
     def _add_config_section(self, config, section_from, index):
-        section_to = section_from + "_%d" % index
+        section_to = section_from[4:] + "_%d" % index
         items = config.fileconfig.items(section_from)
-        new_section = config.fileconfig.add_section(section_to)
+        config.fileconfig.add_section(section_to)
         for item in items:
             if item[0] == 'leds':
                 new_leds = "%s (%d)" % (item[1].split(' ', 1)[0], index)
