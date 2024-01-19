202c75d171386cc47ed6da5a199fc460c999d3ee
Moggie <moggieuk@gmail.com>
2024-01-19 12:59:28 +0700

    Allow for old and new klipper versions

--- extras/mmu_toolhead.py
+++ extras/mmu_toolhead.py
@@ -31,8 +31,12 @@ class MmuToolHead(toolhead.ToolHead, object):
         self.all_mcus = [m for n, m in self.printer.lookup_objects(module='mcu')]
 
         self.mcu = self.all_mcus[0]
-        self.lookahead = toolhead.LookAheadQueue(self) # Happy Hare: Use base class LookAheadQueue
-        self.lookahead.set_flush_time(toolhead.BUFFER_TIME_HIGH) # Happy Hare: Use base class
+        if hasattr(toolhead, 'LookAheadQueue'):
+            self.lookahead = toolhead.LookAheadQueue(self) # Happy Hare: Use base class LookAheadQueue
+            self.lookahead.set_flush_time(toolhead.BUFFER_TIME_HIGH) # Happy Hare: Use base class
+        else:
+            self.move_queue = toolhead.MoveQueue(self) # Happy Hare: Use base class MoveQueue (older klipper)
+            self.move_queue.set_flush_time(toolhead.BUFFER_TIME_HIGH) # Happy Hare: Use base class (older klipper)
         self.commanded_pos = [0., 0., 0., 0.]
 
         self.gear_motion_queue = self.extruder_synced_to_gear = None # Happy Hare: For bi-directional syncing of gear and extruder
