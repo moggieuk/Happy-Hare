147f52a4774be7b99d36906f6fe0d11de9751e95
Moggie <moggieuk@gmail.com>
2024-06-26 20:02:34 +0700

    Fix for issue #316. Allow dots in directory name

--- install.sh
+++ install.sh
@@ -249,9 +249,9 @@ self_update() {
 function nextfilename {
     local name="$1"
     if [ -d "${name}" ]; then
-        printf "%s-%s" ${name%%.*} $(date '+%Y%m%d_%H%M%S')
+        printf "%s-%s" ${name} $(date '+%Y%m%d_%H%M%S')
     else
-        printf "%s-%s.%s-old" ${name%%.*} $(date '+%Y%m%d_%H%M%S') ${name#*.}
+        printf "%s-%s.%s-old" ${name%.*} $(date '+%Y%m%d_%H%M%S') ${name##*.}
     fi
 }
 
