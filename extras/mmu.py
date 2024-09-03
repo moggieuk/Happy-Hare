8a6ce06fbc61ee12a9c5eee1f171400a18180241
Moggie <moggieuk@gmail.com>
2024-09-04 01:39:37 +0700

    simplified pause cmd

--- extras/mmu.py
+++ extras/mmu.py
@@ -6346,12 +6346,10 @@ class Mmu:
     def cmd_PAUSE(self, gcmd):
         self._log_to_file(gcmd.get_commandline())
         if self.is_enabled:
-            self.log_debug("MMU PAUSE wrapper called")
             self._fix_started_state() # Get out of 'started' state before transistion to pause
+            self.log_debug("MMU PAUSE wrapper called")
             self._save_toolhead_position_and_lift("pause", z_hop_height=self.z_hop_height_error)
-            self._wrap_gcode_command("__PAUSE", None) # User defined or Klipper default behavior
-        else:
-            self._wrap_gcode_command("__PAUSE", None) # User defined or Klipper default behavior
+        self._wrap_gcode_command("__PAUSE", None) # User defined or Klipper default behavior
 
     # Not a user facing command - used in automatic wrapper
     cmd_CLEAR_PAUSE_help = "Wrapper around default CLEAR_PAUSE macro"
