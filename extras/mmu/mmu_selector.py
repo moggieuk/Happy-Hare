aa0fda7077015b30b670331afb6eb9a0ce1f6597
Branden Cash <203336+ammmze@users.noreply.github.com>
2025-03-14 20:45:34 -0700

    fix(ServoSelector): initialize selector_bypass_angle with 0
    Otherwise during klipper start up the has_bypass method gets called before this get loaded from the saved variables

--- extras/mmu/mmu_selector.py
+++ extras/mmu/mmu_selector.py
@@ -1502,6 +1502,7 @@ class ServoSelector(BaseSelector, object):
         super(ServoSelector, self).__init__(mmu)
         self.is_homed = True
         self.servo_state = self.mmu.FILAMENT_UNKNOWN_STATE
+        self.selector_bypass_angle = 0
 
         # Get hardware
         servo_name = mmu.config.get('selector_servo_name', "selector_servo")
