c8d1bf7f20fd0643547282d1c6a3795b4146c718
ningpj <36124687+ningpj@users.noreply.github.com>
2025-12-03 12:38:47 +1300

    Combine logging and math imports in mmu_sync_feedback_manager

--- extras/mmu/mmu_sync_feedback_manager.py
+++ extras/mmu/mmu_sync_feedback_manager.py
@@ -19,14 +19,13 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 #
-import logging
+import logging, math
 
 # Happy Hare imports
 
 # MMU subcomponent clases
 from .mmu_sync_controller  import SyncControllerConfig, SyncController
 from .mmu_extruder_monitor import ExtruderMonitor
-
 
 class MmuSyncFeedbackManager:
 
