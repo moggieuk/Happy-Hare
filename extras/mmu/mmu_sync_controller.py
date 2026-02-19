d6d5734fc65d12b80816efcefc2d35f6369d7155
moggieuk <32878385+moggieuk@users.noreply.github.com>
2026-02-19 12:06:00 +0700

    Made test of neutral buffer exclude switch type sensors

--- extras/mmu/mmu_sync_controller.py
+++ extras/mmu/mmu_sync_controller.py
@@ -867,7 +867,7 @@ class _FlowguardEngine(object):
             # Arm when we've moved and observed any change in coarse state or know we are near neutral
             changed_state = (state_now != self._arm_last_state)
             moved = abs(self._arm_motion_mm) > 0.0
-            near_neutral = abs(sensor_reading) < cfg.autotune_stable_x_thresh
+            near_neutral = cfg.sensor_type in ("P") and abs(sensor_reading) < cfg.autotune_stable_x_thresh
             if moved and (changed_state or near_neutral):
                 self._armed = True
             else:
