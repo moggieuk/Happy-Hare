fd2630b9f16ea10926b183bbce76e9519951ee7e
burkfers <6268175+burkfers@users.noreply.github.com>
2026-02-24 13:33:27 +0100

    fix crash upon warning during bowden calibration

--- extras/mmu/mmu.py
+++ extras/mmu/mmu.py
@@ -4780,7 +4780,7 @@ class Mmu:
             raise MmuError("Failed to reach extruder '%s' endstop after moving %.1fmm" % (self.extruder_homing_endstop, max_length))
 
         if measured > (max_length * 0.8):
-            self.log_warning("Warning: 80%% of 'extruder_homing_max' was used homing. You may want to increase 'extruder_homing_max'" % self.VARS_MMU_CALIB_BOWDEN_LENGTH)
+            self.log_warning("Warning: 80%% of 'extruder_homing_max' was used homing. You may want to increase 'extruder_homing_max'")
 
         self._set_filament_pos_state(self.FILAMENT_POS_HOMED_EXTRUDER)
         return homing_movement, extra
