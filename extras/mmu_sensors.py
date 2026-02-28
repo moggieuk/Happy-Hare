4e007e3b341ff4debb49bd78838dea340d0ea0af
Moggieuk <moggieuk@gmail.com>
2026-02-28 18:46:48 +0700

    select only the last sample from adc callback

--- extras/mmu_sensors.py
+++ extras/mmu_sensors.py
@@ -270,7 +270,7 @@ class MmuProportionalSensor:
             try:
                 # New klipper (>= v0.13.0-557)
                 self.adc.setup_adc_sample(self._report_time, self._sample_time, self._sample_count)
-                self.adc.setup_adc_callback_samples(self._adc_callback)
+                self.adc.setup_adc_callback(self._adc_callback)
             except TypeError:
                 # A few versions of klipper had these signatures
                 self.adc.setup_adc_sample(self._sample_time, self._sample_count)
@@ -320,8 +320,17 @@ class MmuProportionalSensor:
         if y >  1.0: y =  1.0
         return y
 
+    def _adc_callback(self, *args):
+        # Old klipper: _adc_callback(read_time, read_value)
+        # New klipper: _adc_callback(samples) where samples is a list of (read_time, read_value)
+        if len(args) == 1:
+            samples = args[0]
+            read_time, read_value = samples[-1]
+        elif len(args) == 2:
+            read_time, read_value = args
+        else:
+            raise TypeError("_adc_callback expected (read_time, read_value) or (samples), got %d args" % len(args))
 
-    def _adc_callback(self, read_time, read_value):
         self.value_raw = float(read_value)
         self.value = self._map_reading(read_value) # Mapped & scaled value
         
@@ -329,14 +338,10 @@ class MmuProportionalSensor:
         # TODO really extreme should be determined by is_extreme() in mmu_sync_feedback manager (with hysteresis), but object hasn't been created yet
         # TODO so for now, use absolute extremes
         if abs(self.value) >= 1.0:
-            extreme = abs(self.value) # 1 or -1
+            extreme = 1 if self.value > 0 else -1
             if extreme != self._last_extreme: # Avoid repeated events
                 self._last_extreme = extreme
                 self.printer.send_event("mmu:sync_feedback", read_time, self.value)
-
-    def _adc_callback_samples(self, samples):
-        for read_time, read_value in samples:
-            self._adc_callback(read_time, read_value)
 
     def get_status(self, eventtime):
         return {
