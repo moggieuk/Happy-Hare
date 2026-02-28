289baffe1f52b01df985a0ee4a505ab8d255c71c
wazzup77 <lucja.rugor@gmail.com>
2026-02-28 18:30:56 +0100

    virtual_endstop test

--- extras/mmu_sensors.py
+++ extras/mmu_sensors.py
@@ -512,7 +512,7 @@ class MmuHallSensor:
             },
             insert_remove_in_print,
             button_handler=None,
-            switch_pin=None
+            switch_pin='virtual_endstop'
         )
 
         self.printer.add_object("mmu_hall_sensor %s" % name, self)
