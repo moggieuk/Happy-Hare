ceaa5389f23b1512ca2355f1eddb615079fbc3c3
moggieuk <32878385+moggieuk@users.noreply.github.com>
2026-08-11 19:01:50 +0700

    fixed typo

--- extras/mmu_machine.py
+++ extras/mmu_machine.py
@@ -41,7 +41,7 @@ _NOT_INSTALLED_MSG = (
     "This looks like it is because of a update to Happy Hare v4 while you are still configured for v3\n"
     "To see options please run:\n"
     "  cd ~/Happy-Hare && ./install.sh\n"
-    "If you know you want to stat on v3, run:\n"
+    "If you know you want to stay on v3, run:\n"
     "  cd ~/Happy-Hare && ./install.sh -b v3\n"
     "More details: https://moggieuk.github.io/Happy-Hare-Doc/Upgrade-v3-v4.md"
 )
